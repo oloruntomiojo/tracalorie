ad92f988d676c8cabd9714ae788a088e2292fa63
David <dieavid01@gmail.com>
2020-09-17 22:08:12 +0100

    sum and display total calories

--- js/ui.js
+++ js/ui.js
@@ -1,21 +1,40 @@
-const UI = function () { 
+const UI = function () {
     this.meal = document.getElementById('meal');
     this.calories = document.getElementById('calories');
+    this.initialCalories = 0;
 }
 
 UI.prototype = {
-    displayCaloriesInfo: function () { 
+    // Display all data
+    displayCaloriesInfo: function () {
         let overview = document.querySelector('.overview');
+        const error = document.querySelector('main em');
 
-        overview.innerHTML += `
+        if (this.meal.value !== '' && this.calories.value !== '') {
+            overview.innerHTML += `
                 <li><strong>${this.meal.value}</strong> : <em>${this.calories.value} Calories</em> <a href="#"><i class="fa fa-pencil"></i></a></li>`;
 
-        this.clearInputFields();
-        // this.alertt();
-     },
+            this.caloriesCount(this.calories.value);
 
-     clearInputFields: function () { 
+            this.clearInputFields();
+
+            error.classList.remove('show');
+        } else {
+            error.classList.add('show');
+        }
+    },
+
+    // Clear form fields
+    clearInputFields: function () {
         this.meal.value = '';
         this.calories.value = '';
-      },
+    },
+
+    // Sum all calories inputed
+    caloriesCount: function (calories) {
+        this.initialCalories = this.initialCalories + Number(calories);
+
+        let totalCalories = document.querySelector('.total-calories');
+        totalCalories.textContent = this.initialCalories;
+    }
 }
